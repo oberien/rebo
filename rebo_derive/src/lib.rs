3cc3cb59499dbf9259303ddc1c8efba571c51aa5
oberien <jaro.fietz@gmx.de>
2021-06-30 18:23:59 +0200

    Add derive macro for deriving functions delegating to each enum variant

--- rebo_derive/src/lib.rs
+++ rebo_derive/src/lib.rs
@@ -1,11 +1,13 @@
 use proc_macro_error::{proc_macro_error, abort};
-use syn::{ItemFn, Signature, FnArg, PatType, Ident, ReturnType, Type};
+use syn::{ItemFn, Signature, FnArg, PatType, Ident, ReturnType, Type, DeriveInput, Token, parse_macro_input, Expr, Result, Attribute, Data, DataUnion, DataStruct, Visibility};
 use proc_macro::TokenStream;
+use syn::parse::{Parse, ParseStream};
+use syn::token::{Union, Struct};
 
 #[proc_macro_error]
 #[proc_macro_attribute]
 pub fn function(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(input as ItemFn);
+    let input = parse_macro_input!(input as ItemFn);
     let ItemFn {
         attrs: _,
         vis,
@@ -72,6 +74,82 @@ pub fn function(_args: TokenStream, input: TokenStream) -> TokenStream {
             },
             imp: ::rebo::common::FunctionImpl::Rust(#fn_ident),
         };
+    }.into();
+    // eprintln!("{}", res);
+    res
+}
+
+/// For internal use only
+#[doc(hidden)]
+#[proc_macro_derive(Functions, attributes(function))]
+pub fn functions(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    struct Function {
+        visibility: Visibility,
+        fn_signature: Signature,
+        _eq: Token![=],
+        name: Ident,
+        _arrow: Token![=>],
+        expr: Expr,
+    }
+    impl Parse for Function {
+        fn parse(input: ParseStream) -> Result<Self> {
+            let content;
+            syn::parenthesized!(content in input);
+            Ok(Function {
+                visibility: content.parse()?,
+                fn_signature: content.parse()?,
+                _eq: content.parse()?,
+                name: content.parse()?,
+                _arrow: content.parse()?,
+                expr: content.parse()?,
+            })
+        }
+    }
+
+    let enum_name = input.ident;
+    let generics = input.generics;
+    let functions: Result<Vec<_>> = input.attrs.into_iter()
+        .map(|Attribute { tokens, .. }| syn::parse2::<Function>(tokens))
+        .collect();
+    let functions = match functions {
+        Ok(f) => f,
+        Err(err) => return TokenStream::from(err.to_compile_error())
+    };
+    let variants: Vec<Ident> = match input.data {
+        Data::Enum(e) => e.variants.into_iter()
+            .map(|variant| variant.ident)
+            .collect(),
+        Data::Struct(DataStruct { struct_token: Struct { span }, ..})
+        | Data::Union(DataUnion { union_token: Union { span }, ..}) => abort!(span, "only enums are supported"),
+    };
+    let num_variants = variants.len();
+    let variants = vec![variants; functions.len()];
+
+    let mut function_visibilities = Vec::new();
+    let mut function_signatures = Vec::new();
+    let mut variable_names = Vec::new();
+    let mut exprs = Vec::new();
+    for function in functions {
+        function_visibilities.push(function.visibility);
+        function_signatures.push(function.fn_signature);
+        variable_names.push(vec![function.name; num_variants]);
+        exprs.push(vec![function.expr; num_variants]);
+    }
+
+    let res = quote::quote! {
+        impl #generics #enum_name #generics {
+            #(
+                #function_visibilities #function_signatures {
+                    match self {
+                        #(
+                            #enum_name::#variants(#variable_names) => #exprs,
+                        )*
+                    }
+                }
+            )*
+        }
     }.into();
     // eprintln!("{}", res);
     res
