8438d4c1f8136e9b50e91dde6fe11e4254de0715
oberien <jaro.fietz@gmx.de>
2022-02-06 10:09:40 +0100

    Update to rt-format 0.3.0

--- rebo/src/common/values.rs
+++ rebo/src/common/values.rs
@@ -14,7 +14,6 @@ use crate::typeck::types::SpecificType;
 use std::collections::BTreeMap;
 use std::convert::TryInto;
 use rt_format::{FormatArgument, Specifier};
-use rt_format::parser::ConvertToSize;
 
 pub trait ExternalTypeType {
     type Type: ExternalType;
@@ -243,13 +242,6 @@ impl FormatArgument for Value {
 
     fn fmt_upper_exp(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::UpperExp::fmt(&UpperExpValue(self), f)
-    }
-}
-
-// rebo doesn't support dynamic width formatters (yet?)
-impl ConvertToSize for Value {
-    fn convert(&self) -> Result<usize, ()> {
-        Err(())
     }
 }
 
