77cb28e148e4680e8e173ec607a8dc52ac054efb
oberien <jaro.fietz@gmx.de>
2022-02-16 21:50:15 +0100

    Create ace-editor from rust

---
+++ playground/src/ace_editor.rs
@@ -0,0 +1,71 @@
+use yew::{function_component, use_node_ref, use_effect_with_deps, html, Properties, Callback};
+use web_sys::Element;
+use wasm_bindgen::prelude::wasm_bindgen;
+use serde::Serialize;
+use wasm_bindgen::JsValue;
+use wasm_bindgen::closure::Closure;
+
+#[derive(Properties, PartialEq)]
+pub struct AceEditorProps {
+    pub default_value: String,
+    pub on_change: Callback<String>,
+}
+
+#[function_component(AceEditor)]
+pub fn ace_editor(props: &AceEditorProps) -> Html {
+    let editor_ref = use_node_ref();
+
+    {
+        let editor_ref = editor_ref.clone();
+        let deps = editor_ref.clone();
+        let default_value = props.default_value.clone();
+        let on_change = props.on_change.clone();
+        use_effect_with_deps(move |_| {
+            let editor = ace_edit(
+                &editor_ref.cast::<Element>().unwrap(),
+                JsValue::from_serde(&AceEditorConfig {
+                    mode: "ace/mode/rust".to_string(),
+                    theme: "ace/theme/monokai".to_string(),
+                    selectionStyle: "text".to_string(),
+                    value: default_value,
+                }).unwrap(),
+            );
+
+            let on_change = {
+                let editor = editor.clone();
+                Closure::wrap(Box::new(move || {
+                    on_change.emit(editor.ace_get_value());
+                }) as Box<dyn FnMut()>).into_js_value()
+            };
+            editor.ace_session_on("change".to_string(), on_change);
+            || ()
+        }, deps);
+    }
+
+    return html! {
+        <div class="code" ref={editor_ref}></div>
+    }
+}
+
+#[derive(Serialize)]
+#[allow(non_snake_case)]
+struct AceEditorConfig {
+    mode: String,
+    theme: String,
+    selectionStyle: String,
+    value: String,
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[derive(Clone)]
+    type Editor;
+
+    #[wasm_bindgen(js_name = "ace.edit")]
+    fn ace_edit(node: &Element, cfg: JsValue) -> Editor;
+    #[wasm_bindgen(method, js_name = "session.on")]
+    fn ace_session_on(this: &Editor, target: String, on_change: JsValue);
+    #[wasm_bindgen(method, js_name = "getValue")]
+    fn ace_get_value(this: &Editor) -> String;
+}
+
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,11 +1,12 @@
-use yew::{function_component, html, use_state, Html, Properties, use_effect_with_deps};
-use wasm_bindgen::{JsValue, closure::Closure, prelude::wasm_bindgen};
-use web_sys::Element;
+use yew::{function_component, html, use_state, Html, Properties, use_effect_with_deps, Callback, use_ref};
 use wasm_logger::Config;
 use gloo_storage::{LocalStorage, Storage};
 use log::Level;
 
 mod rebo;
+mod ace_editor;
+
+use ace_editor::AceEditor;
 
 fn main() {
     wasm_logger::init(Config::new(Level::Info));
@@ -16,27 +17,21 @@ const DEFAULT_CODE: &str = r#"print("Hello, world!");"#;
 
 #[function_component(HelloWorld)]
 fn hello_world() -> Html {
-    let value = use_state(|| String::new());
+    let code = use_ref(|| {
+        let code: Result<String, _> = LocalStorage::get("code");
+        code.ok()
+            .and_then(|s| if s.is_empty() { None } else { Some(s) })
+            .unwrap_or_else(|| DEFAULT_CODE.to_string())
+    });
+    let value = use_state(|| (*code).clone());
     let output = use_state(|| String::new());
 
-    {
+    let on_change = {
         let value = value.clone();
-        use_effect_with_deps(move |_| {
-            let code: Result<String, _> = LocalStorage::get("code");
-            let code = code.ok()
-                .and_then(|s| if s.is_empty() { None } else { Some(s) })
-                .unwrap_or_else(|| DEFAULT_CODE.to_string());
-            value.set(code.clone());
-
-            let on_change = {
-                Closure::wrap(Box::new(move |s: String| {
-                    value.set(s);
-                }) as Box<dyn FnMut(String)>).into_js_value()
-            };
-            ace_editor_component_did_mount(on_change, code);
-            || ()
-        }, ());
-    }
+        Callback::from(move |s: String| {
+            value.set(s);
+        })
+    };
 
     {
         let value = value.clone();
@@ -56,7 +51,7 @@ fn hello_world() -> Html {
         <>
         <h1>{ "Rebo Playground" }</h1>
         <div class="outer">
-            <AceEditor />
+            <AceEditor default_value={(*code).clone()} {on_change} />
             <div class="output">
                 <h3>{ "Output: "}</h3>
                 <AnsiOutput text={(*output).clone()} />
@@ -77,20 +72,4 @@ fn ansi_output(props: &AnsiOutputProps) -> Html {
     let pre = gloo_utils::document().create_element("pre").unwrap();
     pre.set_inner_html(&rendered);
     Html::VRef(pre.into())
-}
-
-#[function_component(AceEditor)]
-fn ace_editor() -> Html {
-    let div = gloo_utils::document().create_element("div").unwrap();
-    div.set_class_name("code");
-    ace_editor_create(&div);
-    Html::VRef(div.into())
-}
-
-#[wasm_bindgen(module = "/src/ace-editor.js")]
-extern "C" {
-    #[wasm_bindgen(js_name = "ace_editor_create")]
-    fn ace_editor_create(node: &Element);
-    #[wasm_bindgen(js_name = "ace_editor_component_did_mount")]
-    fn ace_editor_component_did_mount(on_change: JsValue, default_value: String);
 }
