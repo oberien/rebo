96819321052cee45f3d1a63eb76c46f09c63efe1
oberien <jaro.fietz@gmx.de>
2022-02-18 21:48:25 +0100

    Make main component into struct-component from function-component

--- playground/src/bin/app/main.rs
+++ playground/src/bin/app/main.rs
@@ -1,21 +1,136 @@
-use yew::{function_component, html, use_state, Html, Properties, Callback, use_ref, use_mut_ref, UseStateHandle};
+use yew::{function_component, Html, Properties, Component, Context, html};
+use js_sys::{Uint32Array, Atomics};
+use web_sys::{Worker, window, MessageEvent};
+use wasm_bindgen::JsCast;
+use playground::{OutputPayload, CodePayload};
 use gloo_storage::{LocalStorage, Storage};
+use gloo_events::EventListener;
+use ace_editor::AceEditor;
 
 mod ace_editor;
 
-use ace_editor::AceEditor;
-use web_sys::{window, Worker, MessageEvent};
-use wasm_bindgen::{JsCast, closure::Closure};
-use js_sys::{Uint32Array, Atomics, Function};
-use playground::{CodePayload, OutputPayload};
-use std::cell::RefCell;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 fn main() {
     playground::setup();
-    yew::start_app::<HelloWorld>();
+    yew::start_app::<MainComponent>();
+}
+
+const DEFAULT_CODE: &str = r#"print("Hello, world!");"#;
+
+pub enum Msg {
+    WorkerStarted(Uint32Array),
+    WorkerOutput(OutputPayload),
+    WorkerStopped,
+    CodeChanged(String),
+}
+
+#[derive(Properties, Default, PartialEq)]
+pub struct Props {
+
+}
+
+pub struct MainComponent {
+    code: String,
+    output: String,
+    shared_buffer: Uint32Array,
+    worker: Worker,
+    #[allow(unused)]
+    worker_onmessage_event_listener: EventListener,
+}
+
+impl Component for MainComponent {
+    type Message = Msg;
+    type Properties = Props;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let code: Result<String, _> = LocalStorage::get("code");
+        let code = code.ok()
+            .and_then(|s| if s.is_empty() { None } else { Some(s) })
+            .unwrap_or_else(|| DEFAULT_CODE.to_string());
+        let shared_buffer = Uint32Array::default();
+
+        let worker = worker_new();
+        let link = ctx.link().clone();
+        let worker_onmessage_event_listener = EventListener::new(&worker, "message", move |event| {
+            let msg = event.dyn_ref::<MessageEvent>().unwrap();
+            let msg = if let Ok(buf) = msg.data().dyn_into() {
+                log::warn!("worker started");
+                Msg::WorkerStarted(buf)
+            } else if let Some(b) = msg.data().as_bool() {
+                match b {
+                    false => {
+                        unreachable!("worker sent `false`")
+                    }
+                    true => {
+                        log::warn!("worker finished");
+                        Msg::WorkerStopped
+                    }
+                }
+            } else {
+                let out: OutputPayload = serde_wasm_bindgen::from_value(msg.data())
+                    .expect("can't deserialize OutputPayload");
+                Msg::WorkerOutput(out)
+            };
+            link.send_message(msg);
+        });
+
+        MainComponent {
+            code,
+            output: String::new(),
+            shared_buffer,
+            worker,
+            worker_onmessage_event_listener,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::WorkerStarted(shared_buffer) => {
+                self.shared_buffer = shared_buffer;
+                send_code(&self.shared_buffer, &self.worker, &self.code);
+                false
+            }
+            Msg::WorkerOutput(OutputPayload { output, serial }) => {
+                let expected_serial = Atomics::load(&self.shared_buffer, 0)
+                    .expect("can't load atomic");
+                if serial == expected_serial {
+                    self.output.push_str(&output);
+                    true
+                } else {
+                    false
+                }
+            }
+            Msg::WorkerStopped => false,
+            Msg::CodeChanged(code) => {
+                self.code = code;
+                if self.shared_buffer.length() > 0 {
+                    send_code(&self.shared_buffer, &self.worker, &self.code);
+                }
+                self.output.clear();
+                LocalStorage::set("code", &self.code).expect("can't set localStorage");
+                false
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let on_change = ctx.link().callback(|code: String| Msg::CodeChanged(code));
+        html! {
+            <>
+            <h1>{ "Rebo Playground" }</h1>
+            <div class="outer">
+                <AceEditor default_value={self.code.clone()} {on_change} />
+                <div class="output">
+                    <h3>{ "Output: "}</h3>
+                    <AnsiOutput text={self.output.clone()} />
+                </div>
+            </div>
+            </>
+        }
+    }
 }
 
 fn worker_new() -> Worker {
@@ -26,82 +141,7 @@ fn worker_new() -> Worker {
     Worker::new(&url).expect("failed to spawn worker")
 }
 
-const DEFAULT_CODE: &str = r#"print("Hello, world!");"#;
-
-#[function_component(HelloWorld)]
-fn hello_world() -> Html {
-    let initial_code = use_ref(|| {
-        let initial_code: Result<String, _> = LocalStorage::get("code");
-        initial_code.ok()
-            .and_then(|s| if s.is_empty() { None } else { Some(s) })
-            .unwrap_or_else(|| DEFAULT_CODE.to_string())
-    });
-    let output_string = use_mut_ref(|| String::new());
-    let output = use_state(|| String::new());
-    let shared_buffer = use_mut_ref(|| Uint32Array::default());
-    let worker = {
-        let initial_code = initial_code.clone();
-        let output = output.clone();
-        let output_string = output_string.clone();
-        let shared_buffer = shared_buffer.clone();
-        use_mut_ref(move || {
-            let w = worker_new();
-            let worker = w.clone();
-            let onmessage: Function = Closure::wrap(Box::new(move |msg: MessageEvent| {
-                if let Ok(buf) = msg.data().dyn_into() {
-                    log::warn!("worker started");
-                    *shared_buffer.borrow_mut() = buf;
-                    send_code(&*output_string, &*shared_buffer.borrow(), &worker, &output, &*initial_code);
-                } else if let Some(b) = msg.data().as_bool() {
-                    match b {
-                        false => {
-                            unreachable!("worker sent `false`");
-                        }
-                        true => {
-                            log::warn!("worker finished");
-                        }
-                    }
-                } else {
-                    let out: OutputPayload = serde_wasm_bindgen::from_value(msg.data())
-                        .expect("can't deserialize OutputPayload");
-                    let serial = Atomics::load(&*shared_buffer.borrow(), 0)
-                        .expect("can't load atomic");
-                    if out.serial == serial {
-                        output_string.borrow_mut().push_str(&out.output);
-                        output.set(output_string.borrow().clone());
-                    }
-                }
-            }) as Box<dyn Fn(MessageEvent)>).into_js_value().into();
-            w.set_onmessage(Some(&onmessage));
-            w
-        })
-    };
-
-    let on_change = {
-        let output = output.clone();
-        Callback::from(move |s: String| {
-            if shared_buffer.borrow().length() > 0 {
-                send_code(&*output_string, &*shared_buffer.borrow(), &*worker.borrow(), &output, &s);
-            }
-            LocalStorage::set("code", s).expect("can't set localStorage");
-        })
-    };
-
-    return html! {
-        <>
-        <h1>{ "Rebo Playground" }</h1>
-        <div class="outer">
-            <AceEditor default_value={(*initial_code).clone()} {on_change} />
-            <div class="output">
-                <h3>{ "Output: "}</h3>
-                <AnsiOutput text={(*output).clone()} />
-            </div>
-        </div>
-        </>
-    }
-}
-
-fn send_code(output_string: &RefCell<String>, shared_buffer: &Uint32Array, worker: &Worker, output: &UseStateHandle<String>, code: &str) {
+fn send_code(shared_buffer: &Uint32Array, worker: &Worker, code: &str) {
     let serial = 1 + Atomics::add(shared_buffer, 0, 1)
         .expect("can't add atomically");
     let code = CodePayload {
@@ -112,8 +152,6 @@ fn send_code(output_string: &RefCell<String>, shared_buffer: &Uint32Array, worke
     let js_value = serde_wasm_bindgen::to_value(&code)
         .expect("can't serialize CodePayload");
     worker.post_message(&js_value).expect("can't post_message CodePayload");
-    *output_string.borrow_mut() = String::new();
-    output.set(String::new());
 }
 
 #[derive(Properties, PartialEq)]
