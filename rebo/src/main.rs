b3d0fd52293d2f6f573e1c033c162bb57d0942e3
oberien <jaro.fietz@gmx.de>
2023-12-10 22:06:35 +0100

    Make rebo cli error with status code on non-okay result

--- rebo/src/main.rs
+++ rebo/src/main.rs
@@ -1,6 +1,13 @@
+use rebo::ReturnValue;
+
 fn main() {
     env_logger::init();
     let filename = std::env::args().nth(1).unwrap_or_else(|| "test.re".to_string());
     let code = std::fs::read_to_string(&filename).unwrap();
-    rebo::run(filename, code);
+    match rebo::run(filename, code).return_value {
+        ReturnValue::Ok(_) => (),
+        ReturnValue::ParseError => ::std::process::exit(2),
+        ReturnValue::Diagnostics(_) => ::std::process::exit(3),
+        ReturnValue::Panic => ::std::process::exit(4),
+    }
 }
